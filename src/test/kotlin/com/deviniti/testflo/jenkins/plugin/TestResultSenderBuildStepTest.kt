211db8e28130265649f347e1d0f15f80a30c00e8
mmalicki-deviniti <michal.malicki@deviniti.com>
2021-03-24 16:37:29 +0100

    Revert "add cucumber support" Revert "JPTMS-4176 update doc" Revert "JPTMS-4176 update doc, add json info" Revert "JPTMS-4176 update sender version"

--- src/test/kotlin/com/deviniti/testflo/jenkins/plugin/TestResultSenderBuildStepTest.kt
+++ src/test/kotlin/com/deviniti/testflo/jenkins/plugin/TestResultSenderBuildStepTest.kt
@@ -1,33 +1,28 @@
 package com.deviniti.testflo.jenkins.plugin
 
-import com.deviniti.testflo.jira.ImportTestResultsActive
-import com.deviniti.testflo.jira.JiraCredentialsValidationResult
-import com.deviniti.testflo.jira.ValidJiraCredentials
-import com.deviniti.testflo.testsender.*
-import com.github.kittinunf.fuel.core.FuelError
+import com.deviniti.testflo.testsender.ConfigurationField
+import com.deviniti.testflo.testsender.MissingTestPlanKeyStrategy
 import hudson.model.Result
 import hudson.slaves.EnvironmentVariablesNodeProperty
 import hudson.util.Secret
 import org.junit.Rule
 import org.junit.Test
 import org.jvnet.hudson.test.JenkinsRule
-import java.io.File
 
 class TestResultSenderBuildStepTest {
     @Rule
     @JvmField
     var jenkinsRule = JenkinsRule()
-    val jiraURL = "http://localhost:3000"
+    val jiraURL = "url"
     val jiraUsername = "username"
     val jiraPassword = "password"
     val testResultsDirectory = "**/*.xml"
-    val testResultsType = TestResultsType.JUNIT
 
     @Test
     fun `should store configuration`() {
         // given
         var project = jenkinsRule.createFreeStyleProject()
-        val buildStep = TestResultSenderBuildStep(jiraURL, jiraUsername, Secret.fromString(jiraPassword), testResultsDirectory, MissingTestPlanKeyStrategy.FAIL_TASK, testResultsType)
+        val buildStep = TestResultSenderBuildStep(jiraURL, jiraUsername, Secret.fromString(jiraPassword), testResultsDirectory, MissingTestPlanKeyStrategy.FAIL_TASK)
         project.publishersList.add(buildStep)
 
         // when
@@ -35,7 +30,7 @@ class TestResultSenderBuildStepTest {
 
         // then
         jenkinsRule.assertEqualDataBoundBeans(
-                TestResultSenderBuildStep(jiraURL, jiraUsername, Secret.fromString(jiraPassword), testResultsDirectory, MissingTestPlanKeyStrategy.FAIL_TASK, testResultsType),
+                TestResultSenderBuildStep(jiraURL, jiraUsername, Secret.fromString(jiraPassword), testResultsDirectory, MissingTestPlanKeyStrategy.FAIL_TASK),
                 project.publishersList.first()
         )
     }
@@ -44,7 +39,7 @@ class TestResultSenderBuildStepTest {
     fun `should log errors when required config is missing`() {
         // given
         val project = jenkinsRule.createFreeStyleProject()
-        val buildStep = TestResultSenderBuildStep("", "", Secret.fromString(""), "test", MissingTestPlanKeyStrategy.FAIL_TASK, testResultsType)
+        val buildStep = TestResultSenderBuildStep("", "", Secret.fromString(""), "test", MissingTestPlanKeyStrategy.FAIL_TASK)
         project.publishersList.add(buildStep)
 
         // when
@@ -68,7 +63,7 @@ class TestResultSenderBuildStepTest {
         env[ConfigurationField.TARGET_ITERATION.fieldName] = "CURRENT_ITERATION"
         env[ConfigurationField.TEST_CASE_CREATION_STRATEGY.fieldName] = "CREATE_AND_UPDATE"
         jenkinsRule.jenkins.globalNodeProperties.add(prop)
-        val buildStep = TestResultSenderBuildStep(jiraURL, jiraUsername, Secret.fromString(jiraPassword), "*.xml", MissingTestPlanKeyStrategy.SKIP_TASK, testResultsType)
+        val buildStep = TestResultSenderBuildStep(jiraURL, jiraUsername, Secret.fromString(jiraPassword), "*.xml", MissingTestPlanKeyStrategy.SKIP_TASK)
         project.publishersList.add(buildStep)
         val workspace = jenkinsRule.jenkins.getWorkspaceFor(project)
         workspace!!.createTextTempFile("test", ".xml", "test")
@@ -94,7 +89,7 @@ class TestResultSenderBuildStepTest {
         env[ConfigurationField.TARGET_ITERATION.fieldName] = "CURRENT_ITERATION"
         env[ConfigurationField.TEST_CASE_CREATION_STRATEGY.fieldName] = "CREATE_AND_UPDATE"
         jenkinsRule.jenkins.globalNodeProperties.add(prop)
-        val buildStep = TestResultSenderBuildStep(jiraURL, jiraUsername, Secret.fromString(jiraPassword), "*.xml", MissingTestPlanKeyStrategy.FAIL_TASK, testResultsType)
+        val buildStep = TestResultSenderBuildStep(jiraURL, jiraUsername, Secret.fromString(jiraPassword), "*.xml", MissingTestPlanKeyStrategy.FAIL_TASK)
         project.publishersList.add(buildStep)
         val workspace = jenkinsRule.jenkins.getWorkspaceFor(project)
         workspace!!.createTextTempFile("test", ".xml", "test")
@@ -116,14 +111,11 @@ class TestResultSenderBuildStepTest {
         val project = jenkinsRule.createFreeStyleProject()
         val prop = EnvironmentVariablesNodeProperty()
         val env = prop.envVars
-        env[ConfigurationField.JIRA_URL.fieldName] = "http://localhost:2000"
         env[ConfigurationField.TEST_PLAN_KEY.fieldName] = "tp-123"
         env[ConfigurationField.TARGET_ITERATION.fieldName] = "CURRENT_ITERATION"
         env[ConfigurationField.TEST_CASE_CREATION_STRATEGY.fieldName] = "CREATE_AND_UPDATE"
         jenkinsRule.jenkins.globalNodeProperties.add(prop)
-        val buildStep = TestResultSenderBuildStep(jiraURL, jiraUsername, Secret.fromString(jiraPassword), "*.xml", MissingTestPlanKeyStrategy.FAIL_TASK, testResultsType)
-        testResultSender = TestResultSenderImpl(mockClient)
-
+        val buildStep = TestResultSenderBuildStep(jiraURL, jiraUsername, Secret.fromString(jiraPassword), "*.xml", MissingTestPlanKeyStrategy.FAIL_TASK)
         project.publishersList.add(buildStep)
         val workspace = jenkinsRule.jenkins.getWorkspaceFor(project)
         workspace!!.createTextTempFile("test", ".xml", "test")
@@ -138,19 +130,5 @@ class TestResultSenderBuildStepTest {
         jenkinsRule.assertLogNotContains(TestResultSenderBuildStep.BLANK_JIRA_PASSWORD, build)
         jenkinsRule.assertLogNotContains(TestResultSenderBuildStep.getEmptyTestResultsLabel("*.xml"), build)
         jenkinsRule.assertLogNotContains(TestResultSenderBuildStep.MISSING_TEST_PLAN_KEY, build)
-    }
-
-    private val mockClient = object : com.deviniti.testflo.jira.JiraRestClient {
-        override fun isImportTestResultsActive(configuration: Configuration): ImportTestResultsActive {
-            return ImportTestResultsActive(false, null)
-        }
-
-        override fun sendTestResultFileAsync(configuration: Configuration, testResultZipFile: File): FuelError? {
-            return null
-        }
-
-        override fun validateJiraCredentials(jiraUrl: String, username: String, password: String): JiraCredentialsValidationResult {
-            return ValidJiraCredentials
-        }
     }
 }
